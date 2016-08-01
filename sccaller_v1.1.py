3445326e1ed0764a621ad77f33addfa1b76c8469
Xiao Dong <biosinodx@gmail.com>
2016-07-31 20:45:13 -0400

    Update sccaller_v1.1.py

--- sccaller_v1.1.py
+++ sccaller_v1.1.py
@@ -25,7 +25,7 @@ import math
 import numpy as np
 
 #Parse commandline arguments
-parser=argparse.ArgumentParser(description="sccaller.py, v1.0, Xiao Dong, biosinodx@gmail.com, xiao.dong@einstein.yu.edu")
+parser=argparse.ArgumentParser(description="sccaller.py, v1.1, Xiao Dong, biosinodx@gmail.com, xiao.dong@einstein.yu.edu")
 parser.add_argument("-a", "--analysis", type=str,required=True, help="{hsnp, varcall, cutoff}")
 
 parser.add_argument("-i", "--input", type=str,required=True, help="The input file, required. {hsnp, varcall} mpileup file generated using 'samtools mpileup -C50 -Osf refgenome.fa input.bam > input.mpileup', samtools version 1.3 was tested good; {cutoff}: bedfile generated from {varcall}")
